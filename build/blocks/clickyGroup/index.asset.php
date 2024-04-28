3d2e635cd7cfb1d42fb56c91e5eee46406b5effc
Kris Rutherford <klruther1@gmail.com>
2024-04-28 17:01:33 -0400

    Remove trash css from button group

--- build/blocks/clickyGroup/index.asset.php
+++ build/blocks/clickyGroup/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7ba9c1267b88af51ef79');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0cf813d259ace75def89');
